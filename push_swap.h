d734b05208d2950d87b70476c5579b88397cc674
dimenoste <65079536+dimenoste@users.noreply.github.com>
2026-03-06 18:54:26 +0100

    projet ok

--- push_swap.h
+++ push_swap.h
@@ -6,19 +6,19 @@
 /*   By: mberraho <mehdi.berraho@learner.42.tech    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 18:38:32 by yasmine.aic       #+#    #+#             */
-/*   Updated: 2026/03/05 22:05:53 by mberraho         ###   ########.fr       */
+/*   Updated: 2026/03/06 18:47:02 by mberraho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include "ft_stack.h"
-# include "ft_parser.h"
-# include "ft_bench_strat.h"
+//# include <stdio.h>
 # include "ft_algo.h"
+# include "ft_bench_strat.h"
+# include "ft_parser.h"
 # include "ft_printf.h"
+# include "ft_stack.h"
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
